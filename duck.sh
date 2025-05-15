a778d74ce1fd70b900205b261196d4a645b95ba2
LucasPereiraSouza <lucaspereiradesouza2163@outlook.com>
2025-05-15 09:24:47 -0400

    git feat (duck.sh): Log functionality

--- duck.sh
+++ duck.sh
@@ -6,31 +6,46 @@ set -e
 date=$(date '+%H:%M:%S-%m-%d-%Y')
 backup="duckDNS-curl-$date.log"
 file_path="./duckDNS_logs"
+token_path=./token
+tokenValue=""
+
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
 
-echo "Fedora installed Package Logger v1.0"
-dir_exists() {
-    if [[ -e "$file_path" ]]; then
-        return 0 
-    else
-        echo "File does not exit... Creating one"
-        mkdir "$file_path"
-        return 1 
-    fi
+hasToken(){
+	if [ -f $token_path ]; then
+		tokenValue=$(cat "$token_path")
+        echo $tokenValue
+        return 0;
+	else
+        echo "lacks token file, please do "touch token" in the repository and insert your token file\n"
+		return 1;
+	fi
 }
 
-if dir_exists; then
-    echo "Logging installed packages to $backup..."
-    
-    echo url="https://www.duckdns.org/update?domains=sdautomacoes&token=925049d4-9d8d-42b8-9d4e-c2905a9203fc&ip=" 
-    | curl -k -o $file_path/$backup -K - ;
-    cat ~/duckdns/duck.log
+dir_exists() {
+    	if [ -e "$file_path" ]; then
+        	return 0 
+    	else
+        	echo "File does not exit... Creating one"
+        	mkdir "$file_path"
+        	return 1 
+    	fi
+}
 
-fi
+    hasToken
+    dir_exists
+
+    echo "Fedora installed Package Logger v1.0"
+    echo "Logging installed packages to $backup..."
+    echo $tokenValue
+    echo url="https://www.duckdns.org/update?domains=sdautomacoes&token=$tokenValue&ip=" | curl -k -o $file_path/$backup -K - ;
+    cat $file_path/$backup
+
+
 
 
 
